76a796cafb7c8435600733b10d882c729b655c48
Pankajmalhan <pankajmalhan30@gmail.com>
2019-03-11 20:06:00 +0530

    comit8

--- src/containers/App.js
+++ src/containers/App.js
@@ -39,7 +39,7 @@ class App extends Component {
     })
     return (
       <div className='tc'>
-        <h1 className='f1'>RoboFriends7</h1>
+        <h1 className='f1'>RoboFriends8</h1>
         <SearchBox searchChange={onSearchChange}/>
         <Scroll>
           { isPending ? <h1>Loading</h1> :
